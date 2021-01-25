912d8f8f8b017173362a8d1aa58019a91b67aaba
Ezgi Akar <ezgiakar.ea1@gmail.com>
2021-01-25 18:09:38 +0300

    Add project files.

---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using System;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager baseCustomerManager = new StarbucksCustomerManager(new CustomerCheckManager());
+            baseCustomerManager.Save(new Customer
+            {
+                DateOfBirth = new DateTime(1996, 10, 3),
+                FirstName = "Ezgi",
+                LastName = "Akar",
+                NationalityId = "12345678910"
+            });
+        }
+    }
+}
